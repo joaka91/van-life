05959e5596bec2041a72b4e739ea73576b3b681b
Joakim Karlsson <92369909+joaka91@users.noreply.github.com>
2025-03-15 22:03:52 +0100

    Host van list and detail page layout and styling

--- src/pages/host/HostVan.jsx
+++ src/pages/host/HostVan.jsx
@@ -1,3 +1,40 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router"
+import Badge from "../../components/Badge"
+
 export default function HostVan() {
-  return <h1>host van page</h1>
+  const [van, setVan] = useState(null)
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Response status: ${res.status}`)
+        }
+        return res.json()
+      })
+      .then(json => setVan(json.vans[0]))
+      .catch(e => console.error(e))
+  }, [])
+
+  return (
+    <>
+      {van && <div className="host-van flow">
+        <div className="host-van__top">
+          <div className="host-van__image"><img src={van.imageUrl} alt="Van" /></div>
+          <div className="host-van__top-content">
+            <Badge variant={van.type}>{van.type}</Badge>
+            <h1 className="host-van__name">{van.name}</h1>
+            <p className="host-van__price">${van.price}<span>/day</span></p>
+          </div>
+        </div>
+        <div className="host-van__nav">(Navbar goes here)</div>
+        <p><strong>Name:</strong> {van.name}</p>
+        <p><strong>Category:</strong> <span style={{textTransform: "capitalize"}}>{van.type}</span></p>
+        <p><strong>Description:</strong> {van.description}</p>
+        <p><strong>Visibility:</strong> Public</p>
+      </div>}
+    </>
+  )
 }
--- src/pages/host/HostVans.jsx
+++ src/pages/host/HostVans.jsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router"
+
 export default function HostVans() {
-  return <h1>host vans page</h1>
+  const [vans, setVans] = useState([])
+
+  useEffect(() => {
+    fetch("/api/host/vans")
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Response status: ${res.status}`)
+        }
+        return res.json()
+      })
+      .then(json => setVans(json.vans))
+      .catch(e => console.error(e))
+  }, [])
+
+  const vanItems = vans.map(van => (
+    <div className="host-van-item" key={van.id}>
+      <Link to={`/host/vans/${van.id}`}>
+        <div className="host-van-item__inner">
+          <div className="host-van-item__image"><img src={van.imageUrl} alt="Van" /></div>
+          <div className="host-van-item__content">
+            <p className="host-van-item__name">{van.name}</p>
+            <p className="host-van-item__price">${van.price}/day</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  ))
+
+  return (
+    <>
+      <h1>Your listed vans</h1>
+      {vans && <div className="host-van-list">
+        {vanItems}
+      </div>}
+    </>
+  )
 }
