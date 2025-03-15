b04d251c57cf82ad866aba01caa8e8541df56006
Joakim Karlsson <92369909+joaka91@users.noreply.github.com>
2025-03-15 22:04:00 +0100

    Update HostLayout.jsx

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -2,7 +2,7 @@ import { NavLink, Outlet } from "react-router";
 
 export default function HostLayout() {
   return (
-    <div className="padded">
+    <div className="padded flow">
       <nav>
         <NavLink to="/host" end>Dashboard</NavLink>
         <NavLink to="/host/income">Income</NavLink>
